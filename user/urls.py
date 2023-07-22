10e1b558860a9f98162b58859bd1538705a1d427
will <wp20131020ad@gmail.com>
2023-07-22 13:38:24 +0800

    'login'

--- user/urls.py
+++ user/urls.py
@@ -18,5 +18,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.user_register,name="register"),
+    path("register/", views.user_register,name="register"),
+    path("login/", views.user_login,name="login"),
+    path("logout/",views.user_logout,name="logout"),
+    path("", views.user_profile,name="profile"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,7 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login , logout, authenticate
 
+def user_logout(request):
+    logout(request)
+    return redirect("login")
+
+
+def user_profile(request):
+    user=request.user
+    return render(request,"./user/profile.html",{"user":user})
+
+def user_login(request):
+    message=""
+    if request.method =="POST":
+        if request.POST.get('register'):
+            return redirect('register')
+        username=request.POST.get("username")
+        password=request.POST.get("password")
+
+        print(username,password)
+        if not User.objects.filter(username=username):
+            message="查無此帳號"
+        else:
+            user = authenticate(username=username,password=password)
+            if user is not None:
+                login(request,user)
+                message="登入成功!"
+                return redirect("profile")
+            else:
+                message="密碼錯誤!"
+            
+    return render(request,"./user/login.html",{"message":message})
 # Create your views here.
 def user_register(request):
-    return render(request,"./user/register.html")
+    message=''
+    if request.method == "POST":
+        username=request.POST.get('username')
+        password1=request.POST.get('password1')
+        password2=request.POST.get('password2')
+
+        if len(password1)<8:
+            message= "密碼過短"
+        elif password1!=password2:
+            message= "兩次密碼不相同"
+        else:
+            if User.objects.filter(username=username):
+                message = "帳號已經註冊過"
+            else:
+                user=User.objects.create_user(username=username,password=password1)
+                user.save()
+                message="帳號註冊成功!"
+                login(request,user)
+                return redirect("profile")
+        print(username,password1,password2)
+
+    form= UserCreationForm()
+    return render(request,"./user/register.html",{"form":form, "message":message})
 
 
